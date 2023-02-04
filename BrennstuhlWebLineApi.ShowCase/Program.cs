98cb8df2dbed5476e099097ec0c92b4186d05ad3
Michael Rohrhirsch <michael.rohrhirsch.dev@outlook.de>
2023-02-04 19:03:49 +0100

    add user secrets to showcase

--- BrennstuhlWebLineApi.ShowCase/Program.cs
+++ BrennstuhlWebLineApi.ShowCase/Program.cs
@@ -1,11 +1,16 @@
 ﻿using System.Net;
 using BrennstuhlWebLineApi;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
 
 IEnumerable<Device> devices = await BrennstuhlWebLineFinder.FindAsync(async m =>
                                         {
                                             Console.WriteLine(m.IpAddress.ToString());
 
-                                            m.AddCredential(new NetworkCredential("admin", "admin"));
+                                            m.AddCredential(new NetworkCredential(config["username"], config["password"]));
                                             Console.WriteLine(await m.GetStateInformationAsync());
                                             Console.WriteLine(await m.GetHeaderInformationAsync());
                                         });
